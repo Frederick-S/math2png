aaab9af27e68f697e560268e697290cd60909858
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-28 17:29:54 +0800

    Lint with standard

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require('express')
-const mathjax = require("mathjax-node-svg2png")
+const mathjax = require('mathjax-node-svg2png')
 const app = express()
 
 mathjax.start()
 
 app.get('/', (request, response) => {
-    const math = request.query.math
+  const math = request.query.math
 
-    mathjax.typeset({
-        math,
-        format: 'TeX',
-        png: true,
-        scale: 2
-    }, (data) => {
-        if (data.errors) {
-            console.log(data.errors)
+  mathjax.typeset({
+    math,
+    format: 'TeX',
+    png: true,
+    scale: 2
+  }, (data) => {
+    if (data.errors) {
+      console.log(data.errors)
 
-            response.status(500).end('TeX parse error')
-        } else {
-            const png = new Buffer(data.png.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+      response.status(500).end('TeX parse error')
+    } else {
+      const png = Buffer.from(data.png.replace(/^data:image\/\w+;base64,/, ''), 'base64')
 
-            response.contentType('image/png')
-            response.end(png)
-        }
-    })
+      response.contentType('image/png')
+      response.end(png)
+    }
+  })
 })
 
 app.listen(3000, () => console.log('App listening on port 3000!'))
