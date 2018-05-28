598fcac469573241101c3c0e65d6105cffd08a9c
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-28 16:46:16 +0800

    Convert text to image

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mathjax = require("mathjax-node-svg2png")
+const app = express()
+
+mathjax.start()
+
+app.get('/', (request, response) => {
+    const math = request.query.math
+
+    mathjax.typeset({
+        math,
+        format: 'TeX',
+        png: true,
+        scale: 2
+    }, (data) => {
+        if (data.errors) {
+            response.status(500).end('TeX parse error')
+        } else {
+            const png = new Buffer(data.png.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+
+            response.contentType('image/png')
+            response.end(png)
+        }
+    })
+})
+
+app.listen(3000, () => console.log('App listening on port 3000!'))
